ad5460f6899ddbdf373ae9c3586abcf47397b937
nan <139199228@qq.com>
2018-06-06 23:27:10 +0800

    点击加小手

--- src/index.js
+++ src/index.js
@@ -1,21 +1,83 @@
+// class ParentButton {
+//     constructor(text) {
+//         this.text = text
+//     }
+//     map(f) {
+//         return ParentButton.of(f(this.text))
+//     }
+// }
+// ParentButton.of = function (val) {
+//     return new ParentButton(val)
+// }
+// let ind = 0
+
+// function containerClick(el) {
+//     if (el.target.className === "content_con") {
+//         console.log(el)
+//         ind = el.target.innerHTML;
+//         ind++
+//         el.target.innerHTML = ind;
+//         console.log(ind)
+//     } else {
+//         return
+//     }
+// }
+// container.addEventListener("click", containerClick)
+
+
 class ParentButton {
-    constructor(text) {
-        this.text = text
+    constructor(ele) {
+        this.ind = 0;
+        this.event(this.el(ele), "click", this.containerClick.bind(this))
     }
-    map(f) {
-        return ParentButton.of(f(this.text))
+    el(ele) {
+        const container = document.querySelector(ele)
+        return container
+    }
+    event(el, type, cb) {
+        el.addEventListener(type, cb)
+    }
+    containerClick(el) {
+        if (el.target.className === "content_con") {
+            this.ind = el.target.innerHTML;
+            this.ind++;
+            el.target.innerHTML = this.ind;
+        } else {
+            return
+        }
+
+    }
+    removeEle(el) {
+        if (el.parentNode != null) {
+            console.log(el.parentNode)
+            el.parentNode.removeChild(el)
+        }
     }
 }
-ParentButton.of = function (val) {
-    return new ParentButton(val)
-}
-let ind = 0
+class Thumb extends ParentButton {
+    constructor(ele, thumb) {
+        super(ele)
+        this.thumb = thumb
 
-function containerClick(el) {
-    ind = el.target.innerHTML
-    ind++
-    el.target.innerHTML = ind
+        this.event(super.el(ele), "click", this.createThumb.bind(this))
+    }
+    create() {
+        let div = document.createElement("div")
+        div.classList.add('demoSpan1')
+        return div
+    }
+    createThumb(el) {
+        if (el.path[1].tagName === "LI") {
+            el.path[1].children[1].appendChild(this.create())
+            setTimeout(() => {
+                super.el(this.thumb).classList.add("active")
+                super.event(super.el(".active"), "transitionend", this.transitonEnd)
 
+            }, 400)
+        }
+    }
+    transitonEnd(el) {
+        super.removeEle(el.path[0])
+    }
 }
-let container = document.querySelector(".content")
-container.addEventListener("click", containerClick)
+new Thumb(".content", ".demoSpan1")
